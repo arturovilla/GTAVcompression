bda5ff2c0026219fc63b1285d0451660e718e547
Arturo Villalobos <rturohouse@gmail.com>
2024-06-16 01:19:53 -0500

    finding character frequency

--- arturoHuffman/main.cpp
+++ arturoHuffman/main.cpp
@@ -1,16 +1,23 @@
+#include <cstring>
 #include <iostream>
 #include <fstream>
-#include <list>
 #include <ostream>
 #include <string>
 #include <vector>
+
 // have to make myown tuple struct bc the std lib doesnt have the right 
 // tuple types
 struct Cupling{
-    char characterD;
+    char letter;
     int count;
 };
 
+void printVec(std::vector<Cupling>& input){
+    for(auto & i : input){
+        std::cout<<i.letter;
+        std::cout<<" : " + std::to_string(i.count)<<std::endl;
+    }
+}
 
 int main(){
 
@@ -32,6 +39,24 @@ int main(){
 
     // find the frequency of all the characters
     std::vector<Cupling> frequency;
+    for (char ch : data) {
+        bool found = false;
+        for (auto &element : frequency) {
+            if (element.letter == ch) {
+                element.count += 1;
+                found = true;
+                break;
+            }
+        }
+        if (!found) {
+            Cupling t;
+            t.letter = ch;
+            t.count = 1;  // Initialize count to 1 for new characters
+            frequency.push_back(t);
+        }
+    }
+
+    printVec(frequency);
 
     return 0;
 }
